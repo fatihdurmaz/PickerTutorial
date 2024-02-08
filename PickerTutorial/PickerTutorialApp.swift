b45e44532f588fca66194237377387a7ccfd5b39
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-02-08 14:03:09 +0300

    Initial Commit

---
+++ PickerTutorial/PickerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTutorialApp.swift
+//  PickerTutorial
+//
+//  Created by Fatih Durmaz on 8.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PickerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
